c54c2ad4dcec1585bdcfb4200af329f4d7af08d8
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-03-01 20:40:53 -0500

    use generic base

--- migrations/env.py
+++ migrations/env.py
@@ -16,8 +16,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from database import GuildToSchool
-target_metadata = GuildToSchool.metadata
+from database import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
