13361980c3d2daf09a519af6a719e5eb99038237
Adrian Edwards <adrian@adriancedwards.com>
2024-02-26 15:52:16 -0500

    create db directory if it doesn't exist already. Fixes #5

--- database.py
+++ database.py
@@ -5,11 +5,17 @@ from sqlalchemy.orm import sessionmaker
 from alembic.config import Config
 from alembic import command
 
+from pathlib import Path
+
 # Create an Alembic configuration object
 alembic_cfg = Config("./alembic.ini")  # Replace with the path to your alembic.ini file
 
 # create an SQLite database file and connect to it using SQLAlchemy
-engine = create_engine('sqlite:///database/discord_school_mapping.db')
+db_path_str = 'database/discord_school_mapping.db'
+# Specify the file path
+db_path = Path(f"./{db_path_str}")
+
+engine = create_engine(f'sqlite:///{db_path_str}')
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
@@ -21,6 +27,16 @@ class GuildToSchool(Base):
 
 
 if __name__ == "__main__":
+
+	print("Checking if DB Directory exists...", end="")
+	if not db_path.parent.exists():
+		print("No")
+		print(f"Creating directory at {str(db_path.parent)} ")
+		# Create all directories leading up to the file
+		db_path.parent.mkdir(parents=True, exist_ok=True)
+	else:
+		print("Yes")
+	
 	# create the table in the database if it doesn't already exist
 	print("Creating Database...")
 	Base.metadata.create_all(engine)
