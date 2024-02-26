e9a83622f2f63610f9b1ba833dc82e83a84370e1
Adrian Edwards <adrian@adriancedwards.com>
2024-02-26 15:47:37 -0500

    version stamp the DB on creation. Fixes #4

--- database.py
+++ database.py
@@ -2,6 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from alembic.config import Config
+from alembic import command
+
+# Create an Alembic configuration object
+alembic_cfg = Config("./alembic.ini")  # Replace with the path to your alembic.ini file
+
 # create an SQLite database file and connect to it using SQLAlchemy
 engine = create_engine('sqlite:///database/discord_school_mapping.db')
 Session = sessionmaker(bind=engine)
@@ -16,6 +22,17 @@ class GuildToSchool(Base):
 
 if __name__ == "__main__":
 	# create the table in the database if it doesn't already exist
+	print("Creating Database...")
 	Base.metadata.create_all(engine)
+
+	print("Stamping Database version...")
+
+	with engine.begin() as connection:
+		alembic_cfg.attributes['connection'] = connection
+		# Stamp the database with the latest revision
+
+		command.stamp(alembic_cfg, "head")
+
+	print("Done.")
 
 
